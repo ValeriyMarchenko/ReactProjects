a0949bb58bacff23f163e658f9842156d5cc17d3
Valeriy <valeriy.marchenko02@gmail.com>
2022-12-06 13:01:38 +0300

    Weather app final

---
+++ test-w-app/src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "6db0836cfcmsh1e4d9a6aa6292d5p1960e5jsn2ba7f4aa552e",// enter your rapid api key here
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "8cce2e7a3eefdd7d57722a0791903427"; // enter your key from openweather API
